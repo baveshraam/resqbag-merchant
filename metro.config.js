dc99efb975a376e3d24396a7be12f609960c2247
baveshraam <baveshraamsv@gmail.com>
2026-04-29 20:27:09 +0530

    fix vercel config

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
-
-const config = getDefaultConfig(projectRoot);
-
-// Watch the packages/shared folder
-config.watchFolders = [
-  path.resolve(workspaceRoot, 'packages/shared')
-];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
